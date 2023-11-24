f6cd79ee78d5df9b901dfa26cbf76659359297a2
Tondiki Andika <tondikiandika@Tondikis-MacBook-Air.local>
2023-11-24 16:40:36 +0700

    tes socket

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const Koa = require("koa");
 const cors = require("@koa/cors");
 const json = require("koa-json");
 const bodyParser = require("koa-bodyparser");
+const {Server} = require("socket.io");
+const {createServer} = require("http");
+
 const routes = require("./routes");
 const errorsHandler = require("./middlewares/errorsHandler");
 
@@ -32,11 +35,33 @@ app.use(async (ctx, next) => {
   ctx.set("X-Response-Time", `${ms}ms`);
 });
 
+const socketServer = createServer(app);
+const io = new Server(socketServer, {
+  cors: {
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "domain production"
+        : "http://localhost:3001",
+    methods: ["GET", "POST"],
+  },
+});
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id.substring(0, 5)}`);
+  socket.on("send_chat", (data) => {
+    console.log(data);
+    socket.broadcast.emit("receive_chat", data);
+  });
+});
+
 app.use(routes.routes());
 app.use(routes.allowedMethods());
 
 app.on("error", errorsHandler);
 
 app.listen(port, () => console.log(`Server started at port ${port}...`));
+
+socketServer.listen(3003, () =>
+  console.log(`Socket Server started at port ${3003}...`)
+);
 
 module.exports = app;
