fff2ea9daa79c02c5bdb785f5c010585749641c7
Tondiki Andika <tondikiandika@Tondikis-MacBook-Air.local>
2024-02-09 04:05:11 +0700

    try add dialectModule require pg sequelize

--- models/index.js
+++ models/index.js
@@ -12,7 +12,7 @@ const db = {};
 let sequelize;
 if (config.use_env_variable) {
   // '?sslmode=require' additional string if needed for the database url 
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], {...config, dialectModule: require('pg')});
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
